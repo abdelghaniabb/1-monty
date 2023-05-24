4fcd39d295186b859616fc6fccaefce5d74dfcbb
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-24 22:36:56 +0100

    task0-done

--- main.c
+++ main.c
@@ -1,90 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "monty.h"
+#include "main.h"
+/**
+ * main - check code
+ * @argc: argc
+ * @argv: argv
+ * Return: int
+ */
 
-stack_t *stack = NULL; /* Global stack variable */
-
-void push(stack_t **stack, unsigned int line_number) {
-	int value;
-    char *value_str = strtok(NULL, " \t\n");
-    stack_t *new_node;
-
-    if (value_str == NULL) {
-        fprintf(stderr, "L%d: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-
-    value = atoi(value_str);
-
-    new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL) {
-        fprintf(stderr, "Error: Memory allocation failed\n");
-        exit(EXIT_FAILURE);
-    }
-
-    new_node->n = value;
-    new_node->prev = NULL;
-    new_node->next = NULL;
-
-    if (*stack == NULL) {
-        *stack = new_node;
-    } else {
-        new_node->next = *stack;
-        (*stack)->prev = new_node;
-        *stack = new_node;
-    }
-}
-
-void pall(stack_t **stack) {
-    stack_t *current = *stack;
-
-    while (current != NULL) {
-        printf("%d\n", current->n);
-        current = current->next;
-    }
-}
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+	stack_t *stack = NULL;
 	FILE *file;
 	char line[100];
-	int line_number = 1;
-	
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
+	int line_number;
+	char *opcode;
 
-    file = fopen(argv[1], "r");
-    if (file == NULL) {
-        fprintf(stderr, "Failed to open file: %s\n", argv[1]);
-        return EXIT_FAILURE;
-    }
+	if (argc != 2)
+		/*fprintf(stderr, "Usage: %s <filename>\n", argv[0]);*/
+		return (EXIT_FAILURE);
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+		/*fprintf(stderr, "Failed to open file: %s\n", argv[1]);*/
+		return (EXIT_FAILURE);
 
-    
-    
+	line_number = 1;
+	while (fgets(line, sizeof(line), file))
+	{
+		opcode = strtok(line, " \t\n");
+		if (opcode == NULL || opcode[0] == '#')
+		{
+			line_number++;
+			continue;
+		}
 
-    while (fgets(line, sizeof(line), file)) {
-        char *opcode = strtok(line, " \t\n");
+		if (strcmp(opcode, "push") == 0)
+			push(&stack, line_number);
+		else if (strcmp(opcode, "pall") == 0)
+			pall(&stack);
+		else
+		{
+			fprintf(stderr, "L%d: Unknown opcode: %s\n", line_number, opcode);
+			fclose(file);
+			return (EXIT_FAILURE);
+		}
 
-        if (opcode == NULL || opcode[0] == '#') {
-            line_number++;
-            continue;
-        }
+		line_number++;
+	}
 
-        if (strcmp(opcode, "push") == 0) {
-            push(&stack, line_number);
-        } else if (strcmp(opcode, "pall") == 0) {
-            pall(&stack);
-        } else {
-            fprintf(stderr, "L%d: Unknown opcode: %s\n", line_number, opcode);
-            fclose(file);
-            return EXIT_FAILURE;
-        }
-
-        line_number++;
-    }
-
-    fclose(file);
-    return 0;
+	fclose(file);
+	return (0);
 }
--- task0.c
+++ task0.c
@@ -1,11 +1,20 @@
-#include "monty.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * push - pushes an element to the stack
+ * @stack: the header of the list
+ * @line_number: line_number
+ * Return: void
+ */
 void push(stack_t **stack, unsigned int line_number)
 {
 	int value;
-	char *value_str = strtok(NULL, " \t\n");
+	char *value_str;
 	stack_t *new_node;
 
+	value_str = strtok(NULL, " \t\n");
 	if (value_str == NULL)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
@@ -13,7 +22,7 @@ void push(stack_t **stack, unsigned int line_number)
 	}
 
 	value = atoi(value_str);
-printf("test: %d", value);
+
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
 	{
@@ -26,9 +35,7 @@ printf("test: %d", value);
 	new_node->next = NULL;
 
 	if (*stack == NULL)
-	{
 		*stack = new_node;
-	}
 	else
 	{
 		new_node->next = *stack;
@@ -36,3 +43,21 @@ printf("test: %d", value);
 		*stack = new_node;
 	}
 }
+/**
+ * pall - prints all the values on the stack, starting from the top
+ * @stack: the header of the list
+ *
+ * Return: void
+ */
+void pall(stack_t **stack)
+{
+	stack_t *current;
+
+	current = *stack;
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
+
