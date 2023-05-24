09e9f3f3c97b9c88efbc5a913f7e585a3d5efd28
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-24 22:57:54 +0100

    fddg

--- task0.c
+++ task0.c
@@ -51,9 +51,7 @@ void push(stack_t **stack, unsigned int line_number)
  */
 void pall(stack_t **stack,  unsigned int line_number)
 {
-	stack_t *current;
-
-	current = *stack;
+	stack_t *current = *stack;
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
