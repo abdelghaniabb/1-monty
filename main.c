9931ba5be4ac0a43d52c586623e2cca2565f9714
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-26 00:25:50 +0100

    done1

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "monty.h"
 /**
  * handle - handle opcodes
  * @stack: the head of the list
--- task4.c
+++ task4.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "monty.h"
 
 /**
  * add -  adds the top two elements of the stack
