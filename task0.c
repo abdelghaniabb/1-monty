edc1725df956b4ac341323359b4072562bc14907
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-25 17:44:49 +0100

    chaeck01

--- task0.c
+++ task0.c
@@ -24,7 +24,7 @@ void push(stack_t **stack, unsigned int line_number)
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
 	{
-		fprintf(stderr, "Error: Memory allocation failed\n");
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 
