aced27e47945d0b170e3e6d05acc10c645a78f79
vboxuser <vboxuser@hi510.myguest.virtualbox.org>
2023-05-25 11:02:03 +0100

    gdg

--- task1.c
+++ task1.c
@@ -1,10 +1,5 @@
 #include "main.h"
-/**
- * pint - prints the value at the top of the stack
- * @stack: the head of the list
- * @line_number: line_number
- * Return: void
- */
+
 void pint(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL)
