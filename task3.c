0e66759b64a0be239a48d309db709708091a8720
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-25 18:38:56 +0100

    task3-style

--- task3.c
+++ task3.c
@@ -3,13 +3,13 @@ void swap(stack_t **stack, unsigned int line_number)
 {
 	int temp;
 	
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    temp = (*stack)->n;
-    (*stack)->n = (*stack)->next->n;
-    (*stack)->next->n = temp;
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
 }
